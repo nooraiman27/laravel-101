a5b74c61cd0405c1bef9b302288068d9a2a20f42
Aiman <nooraimanmohd@gmail.com>
2024-01-27 18:30:57 +0800

    chore(migration): add cruds table

---
+++ database/migrations/2024_01_26_135018_create_cruds_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cruds', function (Blueprint $table) {
+            $table->id();
+            $table->string('text');
+            $table->string('email');
+            $table->date('date');
+            $table->decimal('decimal'); // decimal is more precise than double. double is more precise than float\
+            $table->integer('integer');
+            $table->dateTime('datetime');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cruds');
+    }
+};
